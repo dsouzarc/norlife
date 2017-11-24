0101e35884c5ea24764274b849ee4df460a69caf
Ryan D'souza <dsouzarc@gmail.com>
2017-11-24 22:56:45 +0200

    Incomplete HealthKit work

--- Norlife/HealthKitDataManager.m
+++ Norlife/HealthKitDataManager.m
@@ -35,34 +35,47 @@ static HealthKitDataManager *dataManager;
     
     if(self) {
         self.healthStore = [[HKHealthStore alloc] init];
-        NSSet *shareObjectTypes = [NSSet setWithObject:nil];
-        
-        NSSet *readObjectTypes  = [NSSet setWithObjects:
-                                   [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierDistanceWalkingRunning],
-                                   [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierHeartRate],
-                                   [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierFlightsClimbed],
-                                   [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierStepCount],
-                                   [HKObjectType quantityTypeForIdentifier:HK]
-                                   nil];
     }
     
     return self;
 }
 
-+(HKUnit*) heartBeatsPerMinuteUnit
++ (HKUnit*) heartBeatsPerMinuteUnit
 {
     return [[HKUnit countUnit]unitDividedByUnit:[HKUnit minuteUnit]];
 }
 
 - (void) calculateHeartRate
 {
-    [self readHeartRateWithCompletion:^(NSArray *results, NSError *error) {
-        NSString *items = [results componentsJoinedByString:@"\n"];
-        NSLog(@"%@", items);
-    }];
+    NSSet *shareObjectTypes = [[NSSet alloc] init];
+    
+    NSSet *readObjectTypes  = [NSSet setWithObjects:
+                               [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierDistanceWalkingRunning],
+                               [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierHeartRate],
+                               [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierFlightsClimbed],
+                               [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierStepCount], nil];
+    
+    // Request access
+    [self.healthStore requestAuthorizationToShareTypes:shareObjectTypes
+                                        readTypes:readObjectTypes
+                                       completion:^(BOOL success, NSError *error) {
+                                           if(success) {
+                                               NSLog(@"GRANTED");
+                                               [self readHeartRateWithCompletion:^(NSArray *results, NSError *error) {
+                                                   NSString *items = [results componentsJoinedByString:@"\n"];
+                                                   NSLog(@"%@", items);
+                                                   NSLog(@"WE IN");
+                                               }];
+                                           }
+                                           else {
+                                               NSLog(@"NOT ALLOWED TO ACCESS");
+                                           }
+                                       }
+     ];
 }
 
-- (void)readHeartRateWithCompletion:(void (^)(NSArray *results, NSError *error))completion {
+- (void)readHeartRateWithCompletion:(void (^)(NSArray *results, NSError *error))completion
+{
     
     //get the heart rates
     NSDate *now = [NSDate date];
@@ -70,38 +83,48 @@ static HealthKitDataManager *dataManager;
     NSDateComponents *components = [calendar components:NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay fromDate:now];
     components.hour = 0;
     components.minute = 0;
+    components.year = 2016;
     components.second = 0;
     
     NSDate *beginOfDay = [calendar dateFromComponents:components];
     NSPredicate *predicate = [HKQuery predicateForSamplesWithStartDate:beginOfDay endDate:now options:HKQueryOptionStrictStartDate];
     
     HKSampleType *sampleType = [HKSampleType quantityTypeForIdentifier:HKQuantityTypeIdentifierHeartRate];
-    NSSortDescriptor *lastDateDescriptor = [[NSSortDescriptor alloc]
-                                            
-                                            initWithKey:HKSampleSortIdentifierStartDate ascending:YES selector:@selector(localizedStandardCompare:)];
+    NSSortDescriptor *lastDateDescriptor = [[NSSortDescriptor alloc] initWithKey:HKSampleSortIdentifierStartDate
+                                                                       ascending:YES
+                                                                        selector:@selector(localizedStandardCompare:)];
     NSArray *sortDescriptors = @[lastDateDescriptor];
     
-    HKSampleQuery *query = [[HKSampleQuery alloc] initWithSampleType:sampleType predicate:predicate limit:HKObjectQueryNoLimit sortDescriptors:sortDescriptors resultsHandler:^(HKSampleQuery *query, NSArray *results, NSError *error){
+    HKSampleQuery *query = [[HKSampleQuery alloc] initWithSampleType:sampleType
+                                                           predicate:predicate
+                                                               limit:HKObjectQueryNoLimit
+                                                     sortDescriptors:sortDescriptors
+                                                      resultsHandler:^(HKSampleQuery *query, NSArray *results, NSError *error){
         if (!results) {
             NSLog(@"An error occured fetching the user's heartrate. The error was: %@.", error);
             abort();
         }
+                                                          
+                                                          NSLog(@"STILL HERE");
         
         dispatch_async(dispatch_get_main_queue(), ^{
             
             NSMutableArray *arrayHeartRate = [[NSMutableArray alloc]init];
             
             for (HKQuantitySample *sample in results) {
+                NSLog(@"SOME RESULTS");
                 double hbpm = [sample.quantity doubleValueForUnit:[HealthKitDataManager heartBeatsPerMinuteUnit]];
                 [arrayHeartRate addObject:[NSNumber numberWithDouble:hbpm]];
             }
             
             if (completion) {
+                NSLog(@"COMPLETED");
                 completion(arrayHeartRate, error);
             }
         });
     }];
     [[[HKHealthStore alloc] init] executeQuery:query];
+    NSLog(@"QUERY DONE");
     
 }
 
