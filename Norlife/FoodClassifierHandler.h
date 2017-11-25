84f14233d20a559ed0d36d3c3d36a76fce967bb3
Ryan D'souza <dsouzarc@gmail.com>
2017-11-25 09:48:58 +0200

    Ignores constants.json

---
+++ Norlife/FoodClassifierHandler.h
@@ -0,0 +1,13 @@
+//
+//  FoodClassifierHandler.h
+//  Norlife
+//
+//  Created by Ryan D'souza on 11/25/17.
+//  Copyright © 2017 Ryan D'souza. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FoodClassifierHandler : NSObject
+
+@end
---
+++ Norlife/FoodClassifierHandler.m
@@ -0,0 +1,13 @@
+//
+//  FoodClassifierHandler.m
+//  Norlife
+//
+//  Created by Ryan D'souza on 11/25/17.
+//  Copyright © 2017 Ryan D'souza. All rights reserved.
+//
+
+#import "FoodClassifierHandler.h"
+
+@implementation FoodClassifierHandler
+
+@end
